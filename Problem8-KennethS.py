971c38b91707cf4042a114c88c2546aa8cd01862
Phantom-mp4 <73188105+Phantom-mp4@users.noreply.github.com>
2020-11-18 10:50:51 -0600

    Delete Problem8-KennethS.py

--- Problem8-KennethS.py
+++
@@ -1,9 +0,0 @@
-Word1 = input("Give me a word. ")
-
-Word2 = input("Give me another word. ")
-
-if len(Word1) >= len(Word2):
-    print("Word 1 is longer. ")
-
-if len(Word2) >= len(Word1):
-    print("Word 2 is longer. ")
